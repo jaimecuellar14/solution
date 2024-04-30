3d1f13d036c4e8aaeb279c05f7d0728f732f93a1
ONEADNOC\jcuellar <jcuellar@adnoc.ae>
2024-04-30 11:43:25 +0200

    Adding more test cases

--- test.test.js
+++ test.test.js
@@ -2,4 +2,11 @@ const middlewares = require('./app/middlewares/middlewares');
 const clients = require('./app/config/accounts');
 test('Existance of a client account throught IBAN', () => {
     expect(middlewares.checkClient('1', clients)).toBe(false);
+    expect(middlewares.checkClient('GB33BUKB20201555555555', clients)).toBe(true);
+})
+
+
+test('Credict card active test', () => {
+    expect(middlewares.checkIfCardIsActive('GB94BARC10201530093459', clients)).toBe(false);
+    expect(middlewares.checkIfCardIsActive('GB33BUKB20201555555555', clients)).toBe(true);
 })

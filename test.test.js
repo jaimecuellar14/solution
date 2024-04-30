a0274aee87f33771ab50a4c207735cddf740d401
ONEADNOC\jcuellar <jcuellar@adnoc.ae>
2024-04-30 11:40:34 +0200

    Adding parameters to test function

--- test.test.js
+++ test.test.js
@@ -1,5 +1,5 @@
 const middlewares = require('./app/middlewares/middlewares');
 const clients = require('./app/config/accounts');
 test('Existance of a client account throught IBAN', () => {
-    expect(middlewares.checkClient('2', clients).toBe(false))
+    expect(middlewares.checkClient('1', clients)).toBe(false);
 })
